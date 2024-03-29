a6f65d37faabe56febff5b461bd43a5f53834e58
彭莘 <psfott@buaa.edu.cn>
2024-03-29 16:14:53 +0800

    Modify T3-1 游戏结束判断

--- T2/t2-as/tests/index.js
+++ T2/t2-as/tests/index.js
@@ -4,4 +4,5 @@ import {mancalaResult} from "../build/release.js";
 assert.strictEqual(mancalaResult(1,[11,12],2),30001);
 assert.strictEqual(mancalaResult(1,[14],1),20001);
 assert.strictEqual(mancalaResult(1,[13,14,22,23,11,24,15,21],8),20003);
+assert.strictEqual(mancalaResult(1,[13,16,26,12,16,11,25,14,26,24,16,13,16,11,26,25,15,25,16,14,26,24,13,21],24),15022)
 console.log("ok");
--- T3/t3-1-as/tests/index.js
+++ T3/t3-1-as/tests/index.js
@@ -5,4 +5,9 @@ assert.strictEqual(add(1, 2), 3);
 assert.strictEqual(mancalaBoard(1,[13,14,22,23,11,24,15,21],8)[14],2);
 assert.strictEqual(mancalaBoard(1,[14],1)[14],2);
 assert.strictEqual(mancalaBoard(1,[13,16,26],3)[14],1);
+assert.strictEqual(mancalaBoard(1,[
+    13, 16, 22, 21, 15, 25, 16, 12,
+    16, 15, 22, 14, 21, 16, 15, 23,
+    11, 15, 13, 14, 15, 16, 25, 22,
+],24)[14],0);
 console.log("ok");
